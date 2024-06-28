61ee5723859929fa92e620f540b7658b970ac4a7
Albert Ho <albert@picovoice.ai>
2024-06-28 16:28:40 -0700

    add rpi-5 to pipeline

--- resources/scripts/platform.sh
+++ resources/scripts/platform.sh
@@ -30,10 +30,8 @@ case $kernel in
             "0xb76") kernel="raspberry-pi" arch="arm11"$arch_info;;
             "0xc07") kernel="raspberry-pi" arch="cortex-a7"$arch_info ;;
             "0xd03") kernel="raspberry-pi" arch="cortex-a53"$arch_info ;;
-            "0xd07") kernel="jetson" arch="cortex-a57"$arch_info ;;
             "0xd08") kernel="raspberry-pi" arch="cortex-a72"$arch_info ;;
             "0xd0b") kernel="raspberry-pi" arch="cortex-a76"$arch_info ;;
-            "0xc08") kernel="beaglebone" arch=$arch_info ;;
             *) exit 1;;
         esac
     esac
